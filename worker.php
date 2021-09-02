e51ac280a968e77f63b7a93c23e893e2d7762598
wangpeng1208 <286204069@qq.com>
2021-09-03 01:03:48 +0800

    删除文件

--- worker.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('APP_PATH', __DIR__ . '/lib/');
-define('PUBLIC_URL',basename(dirname(__FILE__)));
-define('BIND_MODULE','push/Events');
-// 加载框架引导文件
-require __DIR__ . '/thinkphp/start.php';
